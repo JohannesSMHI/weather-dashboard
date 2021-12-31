6b1802a5721a209459e92bf4176a7f3a0091b92d
Johansson Johannes <a002028@ad.smhi.se>
2021-12-31 14:22:30 +0100

    done

--- windy.py
+++ windy.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 """
-Created on 2021-12-23 16:52
+Created on 2021-12-26 16:47
 
 @author: johannes
 """
-import dash
-from dash import dcc
-from dash import html
-import plotly.express as px
 import pandas as pd
 import numpy as np
-import pathlib
-import time
 
-
-df_example = px.data.wind()
-print(df_example.head())
 
 wind_directions = (
     'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
@@ -43,7 +34,7 @@ def get_speed_range(x):
         return f'{int(np.floor(x))}-{int(np.ceil(x))}'
 
 
-def get_production_dataframe(df):
+def get_windframe(df):
     """Doc."""
     combos = df[['direction', 'strength']].apply(tuple, axis=1)
     unique_combos = sorted(combos.unique())
@@ -61,36 +52,3 @@ def get_production_dataframe(df):
                 data['frequency'].append(0)
 
     return pd.DataFrame(data)
-
-
-PATH = pathlib.Path(__file__).parent.parent
-DATA_PATH = PATH.joinpath("data").resolve()
-
-df = pd.read_feather(DATA_PATH.joinpath('testdata'))
-df['timestamp'] = df['timestamp'].apply(pd.Timestamp)
-
-
-boolean = ~pd.isnull(df['winsp']) & ~pd.isnull(df['windir']) & (df['winsp'] > 0.)
-df_wind = df.loc[boolean, ['winsp', 'windir']].reset_index(drop=True)
-df_wind['direction'] = df_wind['windir'].apply(lambda x: get_direction(x))
-df_wind['strength'] = df_wind['winsp'].apply(lambda x: get_speed_range(x))
-
-df_prod = get_production_dataframe(df_wind)
-
-
-# fig = px.bar_polar(df_prod, r="frequency", theta="direction",
-#                    color="strength", template="plotly_dark",
-#                    color_discrete_sequence=px.colors.sequential.Viridis)
-fig = px.bar_polar(r=df_prod["frequency"], theta=df_prod["direction"],
-                   color=df_prod["strength"], template="plotly_dark",
-                   color_discrete_sequence=px.colors.sequential.Viridis)
-
-
-app = dash.Dash()
-app.layout = html.Div([
-    dcc.Graph(figure=fig)
-])
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
